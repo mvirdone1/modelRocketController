04482e7a5c889b3d9e331ebcee4aea13f234c8ad
Michael Virdone <mvirdone@gmail.com>
2018-09-02 17:02:10 -0400

    Project Setup

---
+++ modelRocketController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		modelRocketController.ino
+ Created:	9/2/2018 4:59:36 PM
+ Author:	mvirdone
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
